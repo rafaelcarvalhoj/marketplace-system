238d60e466268e50bb8eb3f1a0cd11b72947f2d4
Johan Rocha <johanmarcos50@gmail.com>
2022-12-08 14:10:28 -0300

    nova atualizacao

--- produto.c
+++ produto.c
@@ -1,30 +1,42 @@
 #include "loja.h"
 
-FILE *estoque;
-
 typedef struct produto
 {
     char nome[20];
     int estoque, preco, validade[3];
 } produto;
+
 produto produtos;
 int listaProdutos = 0;
 
 void consultarEstoque()
 {
+
 }
 
 
 void adicionarEstoque()
 {
+    FILE *estoque;
+
     printf("Insira o nome do novo produto: ");
-    scanf("%s", produtos.nome);
+    scanf("%[^\n]s", produtos.nome);
     printf("Insira o valor do novo produto: ");
     scanf("%d", &produtos.preco);
     printf("Insira o estoque do novo produto: ");
     scanf("%d", &produtos.estoque);
-    estoque = fopen("estoque.txr", "w");
-    fwrite(produtos, sizeof(struct produto), listaProdutos, estoque);
+    estoque = fopen("estoque.txr", "a+b");
+    if(!estoque) //caso haja erro ao abrir o arquivo, exibe a mensagem e fecha o programa.
+    {
+        fprintf(stderr, "Erro, nao foi possível abrir o arquivo!\n")
+        exit(1);
+    }
+    fwrite(produtos, sizeof(struct produto), listaProdutos, estoque); //por que o contador lista de produtos em fwrite, se vamos escrever um arquivo por vez? o ideal não seria contar para a leitura?
+
+
+
+
+    fclose(estoque);
 }
 
 
@@ -39,7 +51,7 @@ void mainMenuProduto(int *opcao)
     printf("\n=-=-=-=-=Produtos=-=-=-=-=\n");
     printf("(1) Consultar estoque\n");
     printf("(2) Adicionar produtos ao estoque\n");
-    printf("(3) Atualizar valor de produto\n");
+    printf("(3) Alterar produto\n");
     printf("(4) Sair\n");
     scanf("%d", &*opcao);
     switch (*opcao)
@@ -69,7 +81,7 @@ int menuProduto()
     int opcao;
     do
     {
-        mainMenuProduto(&opcao);
+        menuProduto(&opcao);
     } while (opcao != 4);
     return 0;
 }
